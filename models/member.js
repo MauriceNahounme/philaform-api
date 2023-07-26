9abc7b351fc0271d08ff1289f62b583b7be3a88d
MauriceNahounme <mauricenahounme444@gmail.com>
2023-07-26 16:11:26 +0200

    ✅ Update members model

--- models/member.js
+++ models/member.js
@@ -5,6 +5,9 @@ const memberSchema = mongoose.Schema(
   {
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
+    street: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    city: { type: String, required: true },
     email: { type: String, required: true, unique: true, validate: [isEmail] },
     phone: { type: String, required: true },
     gender: { type: String, required: true },
