959c8cd29fb5647c4dc357bfbe769bfc9766ebf5
MauriceNahounme <mauricenahounme444@gmail.com>
2023-07-26 16:31:58 +0200

    ✅  cors

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "https://philaform.netlify.app",
+  })
+);
 
 // Import des routers
 const userRouter = require("./routes/user");
